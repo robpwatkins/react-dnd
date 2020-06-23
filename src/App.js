2e5fc2fb81c4769bbcc5f40353bf7230e27b4638
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-23 17:17:51 -0500

    got a useReducer going

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 import './App.css';
 import DragAndDrop from './DragAndDrop';
 
-function App() {
+const App = () => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_DROP_DEPTH':
+        return { ...state, dropDepth: action.dropDepth};
+      case 'SET_IN_DROP_ZONE':
+        return { ...state, inDropZone: action.inDropZone};
+      case 'ADD_FILE_TO_LIST':
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state; 
+    }
+  };
+  const [data, dispatch] = React.useReducer(
+    reducer, { dropDepth: 0, inDropZone: false, fileList: [] }
+  )
+
   return (
     <div className="App">
       <h1>React drag-and-drop component</h1>
